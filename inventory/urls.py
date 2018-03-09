8b08418fffe3626e2d95232bde39fd1540622c4c
Ed Moehlenpah <ed.moehlenpah@gmail.com>
2018-03-09 15:40:16 -0800

    add more views/templates

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.inventory_list, name='inventory_list'),
     url(r'^inventory/(?P<pk>\d+)/$', views.inventory_detail, name='inventory_detail'),
+    url(r'^inventory/new/$', views.inventory_new, name='inventory_new'),
+    url(r'^inventory/(?P<pk>\d+)/edit/$', views.inventory_edit, name='inventory_edit'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Inventory
+from .forms import InventoryForm
 
 # Create your views here.
 def inventory_list(request):
@@ -11,3 +13,34 @@ def inventory_list(request):
 def inventory_detail(request, pk):
     item = get_object_or_404(Inventory, pk=pk)
     return render(request, 'inventory/inventory_detail.html', {'item': item})
+
+#def inventory_new(request):
+#    form = InventoryForm()
+#    return render(request, 'inventory/inventory_edit.html', {'form': form})
+
+def inventory_new(request):
+    if request.method == "POST":
+        form = InventoryForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.author = request.user
+            item.published_date = timezone.now()
+            item.save()
+            return redirect('inventory_detail', pk=item.pk)
+    else:
+        form = InventoryForm()
+    return render(request, 'inventory/inventory_edit.html', {'form': form})
+
+def inventory_edit(request, pk):
+    item = get_object_or_404(Inventory, pk=pk)
+    if request.method == "POST":
+        form = InventoryForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.author = request.user
+            item.published_date = timezone.now()
+            item.save()
+            return redirect('inventory_detail', pk=item.pk)
+    else:
+        form = InventoryForm(instance=item)
+    return render(request, 'inventory/inventory_edit.html', {'form': form})
