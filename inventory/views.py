0a332fedbd6faf484084f8eeb315c162ef6596bf
Ed Moehlenpah <ed.moehlenpah@gmail.com>
2018-03-09 06:43:46 -0800

    added vie and template for detail and also css

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Inventory
 
 # Create your views here.
 def inventory_list(request):
-    return render(request, 'inventory/inventory_list.html', {})
+    #items = Inventory.objects.filter(title__contains='chair')
+    items = Inventory.objects.all()
+    return render(request, 'inventory/inventory_list.html', {'items': items})
+
+def inventory_detail(request, pk):
+    item = get_object_or_404(Inventory, pk=pk)
+    return render(request, 'inventory/inventory_detail.html', {'item': item})
